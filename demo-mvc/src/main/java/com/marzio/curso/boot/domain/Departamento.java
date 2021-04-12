a8d9d91126e6229c316828998afa68e537f905f6
Marcio <marciotmaz@gmail.com>
2021-04-12 15:07:06 -0300

    add validação cargo e departamento

--- demo-mvc/src/main/java/com/marzio/curso/boot/domain/Departamento.java
+++ demo-mvc/src/main/java/com/marzio/curso/boot/domain/Departamento.java
@@ -3,6 +3,8 @@ package com.marzio.curso.boot.domain;
 import java.util.List;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -13,7 +15,9 @@ import lombok.Setter;
 @Entity
 @Table(name = "DEPARTAMENTOS")
 public class Departamento extends AbstractEntity<Long> {
-	
+
+	@NotBlank(message = " Informe um nome. ")
+	@Size(min = 3, max = 60, message = "O nome do departamento deve ter entre {min} e {max} caracteres. ")
 	@Column(name = "nome" , nullable = false, unique = true, length = 60)
 	private String nome;
 	
